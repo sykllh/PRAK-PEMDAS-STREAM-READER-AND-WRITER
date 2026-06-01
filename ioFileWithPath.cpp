0cf990adedad57b49d0ff3502193479b5f842f1a
sykllh <sykllhidmi@gmail.com>
2026-06-01 14:54:10 +0700

    membuat input untuk nama file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -9,5 +9,7 @@ int main()
     //deklarasi variabel baris dan NamaFile
     string baris, NamaFile;
 
-    
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
 }
