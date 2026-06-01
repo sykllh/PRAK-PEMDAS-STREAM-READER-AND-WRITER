49e802ac0aa1002fc5e54bcb9f6f46da642b2086
sykllh <sykllhidmi@gmail.com>
2026-06-01 14:55:03 +0700

    membuat file dalam mode menulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -12,4 +12,7 @@ int main()
     //membuat input untuk menangkap nama file
     cout << "Masukkan nama file : ";
     cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    offstream outfile;
 }
